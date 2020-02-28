e4053404bbf3cfbc50c53c400b3dbd082b6c1b9d
Roark Habegger <roarkhabegger@gmail.com>
2020-02-28 15:26:55 -0500

    Delete SetEnv1.bat

--- SetEnv1.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build" 
-cd 
-vcvars32.bat &&midl /win32 "C:\Users\roark\OneDrive\Documents\UNC\Polarimeter Research\PolControl\PolarimeterRegistration"\Polcontrol.idl 
