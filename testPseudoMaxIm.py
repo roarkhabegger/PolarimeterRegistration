1af11e78cc5227583272cdd429d9642fe1a02f0a
Roark Habegger <roarkhabegger@gmail.com>
2020-07-29 16:08:10 -0500

    Add files via upload

--- testPseudoMaxIm.py
+++ testPseudoMaxIm.py
@@ -1,3 +1,7 @@
 import win32com.client as com
 myPS = com.Dispatch("PseudoMaxIm")
-myPS.Expose(1.0,0,1)
+print("ReadyForDownload Default Value = ",myPS.ReadyForDownload)
+print("Let's do an exposure!")
+myVal = myPS.Expose(1.0,0,1)
+print("Done Exposing!")
+print("Pose-Expose ReadyForDownload Value = ",myPS.ReadyForDownload)
